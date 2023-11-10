5f69497dd17f1aefb0674002bc6c4ccb17560362
Jagoda Spychala <jagoda.spychala1@gmail.com>
2023-11-10 13:53:56 +0100

    Add functionality for programe: morse function and while loop. Make changes in csv file by adding symbols dictionary.

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import pandas
 
+data = pandas.read_csv("statics/data/morse.csv")
+data_dict = {row.iloc[0]: row.iloc[1] for (index, row) in data.iterrows()}
+
+
+# Convert letters and symbols to the Morse Alphabet
+def morse(user_word):
+    output = []
+    for char in user_word:
+        if char == " ":
+            output.append("(space)")
+        elif char not in data_dict:
+            return "Sorry, you should only enter Latin letters, numbers or some symbols (.'_:;?!–+/()=@)"
+        else:
+            output.append(data_dict[char])
+    return output
+
+
+print("This is a Morse code converter!")
+converter_on = True
+while converter_on:
+    user_input = input('Enter the word you want to convert to Morse code:\n').upper()
+    print(f"The Morse Code of {user_input} is: {morse(user_input)}")
+    next_word = input("Do you want to convert the next word? (Enter yes/no):\n").upper()
+    if next_word == 'NO':
+        converter_on = False
